9f509c6abf6f14b1237b58ed929fddcff2286488
Elia Capiluppi <eliacapiluppi02gmail.com>
2021-10-15 13:00:13 +0200

    utente

---
+++ eseWpf/Classi/Utente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eseWpf.Classi
+{
+    class Utente
+    {
+        private string _Username;
+        private string _Password;
+        private string _NomeCompleto;
+        public Utente(string username,string password,string nomeCompleto)
+        {
+            _Username = username;
+            _Password = password;
+            _NomeCompleto = nomeCompleto;
+        }
+        public string Username 
+        {
+            get
+            {
+                return this._Username;
+            }
+            set
+            {
+                this._Username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this._Password;
+            }
+            set
+            {
+                this._Password = value;
+            }
+        }
+        public string NomeCompleto
+        {
+            get
+            {
+                return this._NomeCompleto;
+            }
+            set
+            {
+                this._NomeCompleto = value;
+            }
+        }
+    }
+}
